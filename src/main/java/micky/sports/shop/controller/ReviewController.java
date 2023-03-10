789e0da431f45329635926cad004f81dc05118b8
dlwlsdud0309 <dlwlsdud9439@gmail.com>
2023-03-10 17:41:14 +0900

    관리자페이지 include 오류 확인

--- src/main/java/micky/sports/shop/controller/ReviewController.java
+++ src/main/java/micky/sports/shop/controller/ReviewController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import micky.sports.shop.service.MickyServiceInter;
 import micky.sports.shop.service.product.ProductDetail_ReviewService;
+import micky.sports.shop.service.review.ReviewAdminManagementreviewService;
 import micky.sports.shop.service.review.ReviewAdminpageService;
 import micky.sports.shop.service.review.ReviewChartService;
 import micky.sports.shop.service.review.ReviewDeleteService;
@@ -208,6 +209,18 @@ public class ReviewController {
 		
 		model.addAttribute("request", request);
 		mickyServiceInter=new ReviewChartService(sqlSession,httpSession);
+		mickyServiceInter.execute(model);
+		
+		return "review/reviewChart";
+	}
+
+//	관리자 리뷰관리페이지
+	@RequestMapping("/reviewAdminManagementreview")
+	public String reviewAdminReviewpage(HttpServletRequest request, Model model) {
+		System.out.println("=====reviewAdminManagementreview====");
+		
+		model.addAttribute("request", request);
+		mickyServiceInter=new ReviewAdminManagementreviewService(sqlSession,httpSession);
 		mickyServiceInter.execute(model);
 		
 		return "review/reviewChart";
